b461d7e100af9042a4724078445d7da4e5e4b075
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-11 12:12:25 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,48 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const cardsGallery = document.querySelector(".gallery");
+const cardsMarkup = createPictureSetMarkup(galleryItems);
+
+cardsGallery.insertAdjacentHTML("beforeend", cardsMarkup);
+cardsGallery.addEventListener("click", onGalleryLightClick);
+
+function createPictureSetMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return (
+            `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}"
+  alt="${description}" />
+</a>
+    `
+        );
+    }).join('');
+}
+
+
+function onGalleryLightClick(e) {
+    e.preventDefault();
+
+    if (!e.target.tagName === "IMG") {
+        return;
+    }
+    
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+
+console.log(galleryItems);
