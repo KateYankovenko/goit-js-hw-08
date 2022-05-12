95a601ce9d7d621eec2502f74e603a9850a0d1da
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-13 00:05:38 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,6 +8,24 @@ iframe.addEventListener("timeupdate", onPlay);
 const onPlay = function(event) {
     console.log("Go");
 };
+
+
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
 // iframe.on('play', onPlay);
 
 // iframe.addEventListener(
