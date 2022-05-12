e5333e797bf14391837a45297cc978e2e5b0ce1b
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-12 12:30:26 +0200

    import library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,51 +1,48 @@
-// import SimpleLightbox from "../node_modules/simplelightbox";
-import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.min.css";
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const cardsGallery = document.querySelector(".gallery");
+const cardsMarkup = createPictureSetMarkup(galleryItems);
+
+cardsGallery.insertAdjacentHTML("beforeend", cardsMarkup);
+cardsGallery.addEventListener("click", onGalleryLightClick);
+
+function createPictureSetMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return (
+            `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}"
+  alt="${description}" />
+</a>
+    `
+        );
+    }).join('');
+}
 
 
+function onGalleryLightClick(e) {
+    e.preventDefault();
 
-
-// import { galleryItems } from './gallery-items.js';
-
-// const cardsGallery = document.querySelector(".gallery");
-// const cardsMarkup = createPictureSetMarkup(galleryItems);
-
-// cardsGallery.insertAdjacentHTML("beforeend", cardsMarkup);
-// cardsGallery.addEventListener("click", onGalleryLightClick);
-
-// function createPictureSetMarkup(galleryItems) {
-
-//     return galleryItems.map(({ preview, original, description }) => {
-//         return (
-//             `
-// <a class="gallery__item" href="${original}">
-//   <img class="gallery__image" 
-//   src="${preview}"
-//   alt="${description}" />
-// </a>
-//     `
-//         );
-//     }).join('');
-// }
-
-
-// function onGalleryLightClick(e) {
-//     e.preventDefault();
-
-//     if (!e.target.tagName === "IMG") {
-//         return;
-//     }
+    if (!e.target.tagName === "IMG") {
+        return;
+    }
     
-// }
+}
 
-// const lightbox = new SimpleLightbox('.gallery a', {
-//         captionsData: "alt",
-//         captionDelay: 250,
-//     });
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
 
 
-// console.log(galleryItems);
+console.log(galleryItems);
