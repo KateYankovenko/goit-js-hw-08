cdfaa58da7faa8ce25ff98136af926b8e34bbe18
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-12 20:13:28 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,3 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector("#vimeo-player");
+// iframe.addEventListener("timeupdate", onPlay);
+
+
+iframe.addEventListener(
+  "timeupdate",
+  _.debounce(() => {
+    console.log("");
+  }, 1000)
+);
+// const onPlay = function(event) {
+    
+// };
+// player.on('play', onPlay);
