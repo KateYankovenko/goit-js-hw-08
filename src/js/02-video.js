85fc5fa5cb8a37399356bad1706e0c43770062a4
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-13 14:46:30 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector("#vimeo-player");
+// Сховище з рядком-ключем
 const STORAGE_KEY = "videoplayer-current-time";
 iframe.addEventListener("timeupdate", onPlay);
 
@@ -11,19 +12,19 @@ const onPlay = function(event) {
 
 
 
-player.setCurrentTime(STORAGE_KEY).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+    player.setCurrentTime(STORAGE_KEY).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+            default:
+                // some other error occurred
+                break;
+        }
+    });
 
 
 // iframe.on('play', onPlay);
