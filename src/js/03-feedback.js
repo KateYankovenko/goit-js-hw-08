7a3b137eb1ec6170c6e0efaccc15c7c96ab75423
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-13 19:11:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input[name=email]");
+const messageInput = document.querySelector("input[name=message]");
+console.log(emailInput);
+console.log(messageInput);
+
+// const LOCALSTORAGE_KEY = "";
